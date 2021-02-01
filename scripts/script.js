26718c6c0dabb1e1657e171c83eebb849385499c
tedo74 <teddy2@abv.bg>
2021-02-01 11:41:37 +0200

    script work

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,19 @@
 const game = document.getElementById('game-menu');
-console.log(game);
+game.addEventListener('click', () => {
+  let el = document.querySelector('.accordion-hide');
+  console.log(el);
+  if (el) {
+    // hidden.forEach((el) => {
+    el.classList.remove('accordion-hide');
+    el.classList.add('accordion-show');
+    // el.style.zIndex = 1003;
+    // el.style.backgroundColor = 'green';
+    // hidden.style.position = 'absolute';
+    //   console.log(el);
+    // });
+  }
+
+  //   game.classList.remove('accordion-hide');
+  //   game.classList.add('accordion-show');
+  //   console.log(game.classList);
+});
